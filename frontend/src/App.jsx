17a403245eb528cdaf4b0798a20ade7f526d95e2
Rajendranv070204 <rajendranv070204@gmail.com>
2024-12-25 13:29:12 +0530

    fe code fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,17 +13,17 @@ const App = () => {
     }, []);
 
     const fetchTodolists = async () => {
-        const response = await axios.get('http://localhost:5001/rj/tasks');
+        const response = await axios.get(`${process.env.BACKEND_URL}/rj/tasks`);
         setTodolist(response.data);
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (editing) {
-            await axios.put(`http://localhost:5001/rj/tasks/${editing}`, { task });
+            await axios.put(`${process.env.BACKEND_URL}/rj/tasks/${editing}`, { task });
             setEditing(null);
         } else {
-            await axios.post('http://localhost:5001/rj/tasks', { task });
+            await axios.post(`${process.env.BACKEND_URL}/rj/tasks`, { task });
         }
         setTask('');
         fetchTodolists();
@@ -38,7 +38,7 @@ const App = () => {
     };
 
     const handleDelete = async (id) => {
-        await axios.delete(`http://localhost:5001/rj/tasks/${id}`);
+        await axios.delete(`${process.env.BACKEND_URL}/rj/tasks/${id}`);
         fetchTodolists();
     };
 
